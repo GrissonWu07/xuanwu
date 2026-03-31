cc-gaojun/atlasclaw

feat(hooks): add context-ready events and script hook runtime

app/atlasclaw/hooks/runtime_builtin.py
@@ -1,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from uuid import uuid4
+
+from app.atlasclaw.hooks.runtime import HookHandlerDefinition, HookRuntime
+from app.atlasclaw.hooks.runtime_models import HookEventType, PendingHookItem
+
+
+RUNTIME_AUDIT_MODULE = "runtime-audit"
+PENDING_EVENT_TYPES = {
+    HookEventType.MESSAGE_USER_CORRECTED,
+    HookEventType.RUN_FAILED,
+    HookEventType.LLM_FAILED,
+    HookEventType.TOOL_FAILED,
+}
+
+
+def register_builtin_hook_handlers(runtime: HookRuntime) -> None:
+    """Register generic built-in handlers for runtime observability."""
+
+    async def runtime_audit_handler(event, runtime_context):
+        await runtime_context.hook_state_store.append_event(RUNTIME_AUDIT_MODULE, event)
+
+        if event.event_type not in PENDING_EVENT_TYPES:
+            return
+
+        summary = _build_pending_summary(event)
+        now = datetime.now(timezone.utc)
+        await runtime_context.hook_state_store.append_pending(
+            RUNTIME_AUDIT_MODULE,
+            PendingHookItem(
+                id=f"pending-{uuid4().hex}",
+                module_name=RUNTIME_AUDIT_MODULE,
+                user_id=event.user_id,
+                source_event_ids=[event.id],
+                summary=summary,
+                payload={
+                    "body": summary,
+                    "event_type": event.event_type.value,
+                    "payload": dict(event.payload),
+                },
+                created_at=now,
+                updated_at=now,
+            ),
+        )
+
+    runtime.register(
+        HookHandlerDefinition(
+            module_name=RUNTIME_AUDIT_MODULE,
+            event_types=set(HookEventType),
+            handler=runtime_audit_handler,
+            priority=100,
+        )
+    )
+
+
+def _build_pending_summary(event) -> str:
+    if event.event_type == HookEventType.MESSAGE_USER_CORRECTED:
+        correction = str(event.payload.get("correction") or event.payload.get("message") or "").strip()
+        return correction or "User provided a correction that may need review"
+    if event.event_type == HookEventType.RUN_FAILED:
+        return str(event.payload.get("error") or "Agent run failed")
+    if event.event_type == HookEventType.LLM_FAILED:
+        return str(event.payload.get("error") or "LLM request failed")
+    if event.event_type == HookEventType.TOOL_FAILED:
+        tool_name = str(event.payload.get("tool_name") or event.payload.get("tool") or "unknown_tool")
+        error = str(event.payload.get("error") or "Tool execution failed")
+        return f"{tool_name}: {error}"
+    return f"Review hook event: {event.event_type.value}"

tests/atlasclaw/test_hook_runtime_config.py
@@ -1,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from app.atlasclaw.core.config_schema import AtlasClawConfig
+
+
+def test_hooks_runtime_config_parses_script_handlers() -> None:
+    config = AtlasClawConfig.model_validate(
+        {
+            "hooks_runtime": {
+                "script_handlers": [
+                    {
+                        "module": "script-audit",
+                        "events": ["run.context_ready", "run.failed"],
+                        "command": ["python", "scripts/hook.py"],
+                        "timeout_seconds": 15,
+                        "enabled": True,
+                        "cwd": ".",
+                        "priority": 50,
+                    }
+                ]
+            }
+        }
+    )
+
+    assert len(config.hooks_runtime.script_handlers) == 1
+    handler = config.hooks_runtime.script_handlers[0]
+    assert handler.module == "script-audit"
+    assert handler.events == ["run.context_ready", "run.failed"]
+    assert handler.command == ["python", "scripts/hook.py"]
+    assert handler.timeout_seconds == 15
+    assert handler.enabled is True
+    assert handler.cwd == "."
+    assert handler.priority == 50

tests/atlasclaw/test_hook_runtime_dispatcher.py
@@ -1,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+
+import pytest
+
+from app.atlasclaw.hooks.runtime import (
+    HookHandlerDefinition,
+    HookRuntime,
+    HookRuntimeContext,
+)
+from app.atlasclaw.hooks.runtime_models import HookDecision, HookEventEnvelope, HookEventType
+from app.atlasclaw.hooks.runtime_sinks import ContextSink, MemorySink
+from app.atlasclaw.hooks.runtime_store import HookStateStore
+
+
+@dataclass
+class _Recorder:
+    seen: list[str] = field(default_factory=list)
+
+    async def __call__(self, event, runtime_context):
+        self.seen.append(event.id)
+        await runtime_context.hook_state_store.append_event("audit", event)
+
+
+@pytest.mark.asyncio
+async def test_hook_runtime_dispatches_registered_handlers(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    runtime = HookRuntime(
+        HookRuntimeContext(
+            workspace_path=str(tmp_path),
+            hook_state_store=store,
+            memory_sink=MemorySink(str(tmp_path)),
+            context_sink=ContextSink(store),
+        )
+    )
+    recorder = _Recorder()
+    runtime.register(
+        HookHandlerDefinition(
+            module_name="audit",
+            event_types={HookEventType.RUN_STARTED},
+            handler=recorder,
+        )
+    )
+
+    event = HookEventEnvelope(
+        id="evt-1",
+        event_type=HookEventType.RUN_STARTED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-1",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"message": "hello"},
+    )
+
+    await runtime.dispatch(event)
+
+    assert recorder.seen == ["evt-1"]
+    stored = await store.list_events("audit", "user-a")
+    assert [item.id for item in stored] == ["evt-1"]
+
+
+@pytest.mark.asyncio
+async def test_hook_runtime_confirm_promotes_memory(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    runtime = HookRuntime(
+        HookRuntimeContext(
+            workspace_path=str(tmp_path),
+            hook_state_store=store,
+            memory_sink=MemorySink(str(tmp_path)),
+            context_sink=ContextSink(store),
+        )
+    )
+    event = HookEventEnvelope(
+        id="evt-2",
+        event_type=HookEventType.RUN_FAILED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-2",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"error": "failure"},
+    )
+
+    pending = await runtime.create_pending_item(
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=[event.id],
+        summary="Review failure",
+        payload={"error": "failure"},
+    )
+    resolved = await runtime.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id=pending.id,
+        decision=HookDecision.CONFIRM.value,
+        decided_by="user-a",
+        note="approved",
+    )
+
+    assert resolved.status.value == "confirmed"
+    memory_files = list((tmp_path / "users" / "user-a" / "memory").glob("memory_*.md"))
+    assert len(memory_files) == 1
+    content = memory_files[0].read_text(encoding="utf-8")
+    assert "Review failure" in content
+    confirmed_items = await runtime.context_sink.list_confirmed("audit", "user-a")
+    assert len(confirmed_items) == 1

tests/atlasclaw/test_hook_runtime_sinks.py
@@ -1,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+
+import pytest
+
+from app.atlasclaw.hooks.runtime_models import HookWriteMemoryRequest, PendingHookItem
+from app.atlasclaw.hooks.runtime_sinks import ContextSink, MemorySink
+from app.atlasclaw.hooks.runtime_store import HookStateStore
+
+
+@pytest.mark.asyncio
+async def test_memory_sink_writes_timestamped_memory_file(tmp_path):
+    sink = MemorySink(workspace_path=str(tmp_path))
+
+    result = await sink.write_confirmed(
+        HookWriteMemoryRequest(
+            module_name="audit",
+            user_id="user-a",
+            title="Promoted Lesson",
+            body="Remember this confirmed lesson.",
+            source_event_ids=["evt-1", "evt-2"],
+            metadata={"kind": "lesson"},
+        )
+    )
+
+    assert result.path.exists()
+    assert result.path.parent == tmp_path / "users" / "user-a" / "memory"
+    content = result.path.read_text(encoding="utf-8")
+    assert "Promoted Lesson" in content
+    assert "source_event_ids: evt-1, evt-2" in content
+    assert "kind: lesson" in content
+
+
+@pytest.mark.asyncio
+async def test_context_sink_exposes_confirmed_items_only(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    now = datetime.now(timezone.utc)
+    await store.append_pending(
+        "audit",
+        PendingHookItem(
+            id="pending-1",
+            module_name="audit",
+            user_id="user-a",
+            source_event_ids=["evt-1"],
+            summary="Confirmed item",
+            payload={"kind": "lesson"},
+            status="confirmed",
+            created_at=now,
+            updated_at=now,
+        ),
+    )
+    await store.append_pending(
+        "audit",
+        PendingHookItem(
+            id="pending-2",
+            module_name="audit",
+            user_id="user-a",
+            source_event_ids=["evt-2"],
+            summary="Still pending",
+            payload={"kind": "candidate"},
+            status="pending",
+            created_at=now,
+            updated_at=now,
+        ),
+    )
+
+    sink = ContextSink(store)
+    confirmed = await sink.list_confirmed("audit", "user-a")
+
+    assert len(confirmed) == 1
+    assert confirmed[0].summary == "Confirmed item"
+    assert confirmed[0].payload["kind"] == "lesson"
+
+
+@pytest.mark.asyncio
+async def test_context_sink_includes_explicit_injections(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    sink = ContextSink(store)
+
+    await sink.add_injection(
+        module_name="audit",
+        user_id="user-a",
+        summary="Recent preference",
+        payload={"body": "Prefer concise summaries"},
+        source_event_ids=["evt-3"],
+    )
+
+    confirmed = await sink.list_confirmed("audit", "user-a")
+
+    assert len(confirmed) == 1
+    assert confirmed[0].summary == "Recent preference"
+    assert confirmed[0].payload["body"] == "Prefer concise summaries"

tests/atlasclaw/test_hook_runtime_store.py
@@ -1,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+
+import pytest
+
+from app.atlasclaw.hooks.runtime_models import (
+    HookEventEnvelope,
+    HookEventType,
+    PendingHookItem,
+)
+from app.atlasclaw.hooks.runtime_store import HookStateStore
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_isolates_events_per_user(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    event = HookEventEnvelope(
+        id="evt-1",
+        event_type=HookEventType.RUN_STARTED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-1",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"message": "hello"},
+    )
+
+    await store.append_event("audit", event)
+
+    user_a_events = await store.list_events("audit", "user-a")
+    user_b_events = await store.list_events("audit", "user-b")
+
+    assert [item.id for item in user_a_events] == ["evt-1"]
+    assert user_b_events == []
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_tracks_pending_item_lifecycle(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    pending = PendingHookItem(
+        id="pending-1",
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=["evt-1"],
+        summary="Review the captured runtime anomaly",
+        payload={"kind": "anomaly"},
+        status="pending",
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc),
+    )
+
+    await store.append_pending("audit", pending)
+    initial_pending = await store.list_pending("audit", "user-a")
+    assert [item.id for item in initial_pending] == ["pending-1"]
+
+    confirmed = await store.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id="pending-1",
+        decision="confirm",
+        decided_by="user-a",
+        note="looks good",
+    )
+
+    assert confirmed.status == "confirmed"
+    assert await store.list_pending("audit", "user-a") == []
+
+    decisions = await store.list_decisions("audit", "user-a")
+    assert len(decisions) == 1
+    assert decisions[0].pending_id == "pending-1"
+    assert decisions[0].decision == "confirm"
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_reject_records_decision_and_hides_pending(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    pending = PendingHookItem(
+        id="pending-2",
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=["evt-2"],
+        summary="Reject this candidate",
+        payload={"kind": "candidate"},
+        status="pending",
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc),
+    )
+
+    await store.append_pending("audit", pending)
+    rejected = await store.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id="pending-2",
+        decision="reject",
+        decided_by="user-a",
+        note="not useful",
+    )
+
+    assert rejected.status == "rejected"
+    assert await store.list_pending("audit", "user-a") == []
+
+    decisions = await store.list_decisions("audit", "user-a")
+    assert len(decisions) == 1
+    assert decisions[0].decision == "reject"

GrissonWu07/xuanwu

(cherry picked from commit 5034c09067735d610b2aba76761781f4de518db5)

app/xuanwu/hooks/runtime_builtin.py
@@ -1,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from uuid import uuid4
+
+from app.xuanwu.hooks.runtime import HookHandlerDefinition, HookRuntime
+from app.xuanwu.hooks.runtime_models import HookEventType, PendingHookItem
+
+
+RUNTIME_AUDIT_MODULE = "runtime-audit"
+PENDING_EVENT_TYPES = {
+    HookEventType.MESSAGE_USER_CORRECTED,
+    HookEventType.RUN_FAILED,
+    HookEventType.LLM_FAILED,
+    HookEventType.TOOL_FAILED,
+}
+
+
+def register_builtin_hook_handlers(runtime: HookRuntime) -> None:
+    """Register generic built-in handlers for runtime observability."""
+
+    async def runtime_audit_handler(event, runtime_context):
+        await runtime_context.hook_state_store.append_event(RUNTIME_AUDIT_MODULE, event)
+
+        if event.event_type not in PENDING_EVENT_TYPES:
+            return
+
+        summary = _build_pending_summary(event)
+        now = datetime.now(timezone.utc)
+        await runtime_context.hook_state_store.append_pending(
+            RUNTIME_AUDIT_MODULE,
+            PendingHookItem(
+                id=f"pending-{uuid4().hex}",
+                module_name=RUNTIME_AUDIT_MODULE,
+                user_id=event.user_id,
+                source_event_ids=[event.id],
+                summary=summary,
+                payload={
+                    "body": summary,
+                    "event_type": event.event_type.value,
+                    "payload": dict(event.payload),
+                },
+                created_at=now,
+                updated_at=now,
+            ),
+        )
+
+    runtime.register(
+        HookHandlerDefinition(
+            module_name=RUNTIME_AUDIT_MODULE,
+            event_types=set(HookEventType),
+            handler=runtime_audit_handler,
+            priority=100,
+        )
+    )
+
+
+def _build_pending_summary(event) -> str:
+    if event.event_type == HookEventType.MESSAGE_USER_CORRECTED:
+        correction = str(event.payload.get("correction") or event.payload.get("message") or "").strip()
+        return correction or "User provided a correction that may need review"
+    if event.event_type == HookEventType.RUN_FAILED:
+        return str(event.payload.get("error") or "Agent run failed")
+    if event.event_type == HookEventType.LLM_FAILED:
+        return str(event.payload.get("error") or "LLM request failed")
+    if event.event_type == HookEventType.TOOL_FAILED:
+        tool_name = str(event.payload.get("tool_name") or event.payload.get("tool") or "unknown_tool")
+        error = str(event.payload.get("error") or "Tool execution failed")
+        return f"{tool_name}: {error}"
+    return f"Review hook event: {event.event_type.value}"

tests/xuanwu/test_hook_runtime_config.py
@@ -1,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from app.xuanwu.core.config_schema import XuanWuConfig
+
+
+def test_hooks_runtime_config_parses_script_handlers() -> None:
+    config = XuanWuConfig.model_validate(
+        {
+            "hooks_runtime": {
+                "script_handlers": [
+                    {
+                        "module": "script-audit",
+                        "events": ["run.context_ready", "run.failed"],
+                        "command": ["python", "scripts/hook.py"],
+                        "timeout_seconds": 15,
+                        "enabled": True,
+                        "cwd": ".",
+                        "priority": 50,
+                    }
+                ]
+            }
+        }
+    )
+
+    assert len(config.hooks_runtime.script_handlers) == 1
+    handler = config.hooks_runtime.script_handlers[0]
+    assert handler.module == "script-audit"
+    assert handler.events == ["run.context_ready", "run.failed"]
+    assert handler.command == ["python", "scripts/hook.py"]
+    assert handler.timeout_seconds == 15
+    assert handler.enabled is True
+    assert handler.cwd == "."
+    assert handler.priority == 50

tests/xuanwu/test_hook_runtime_dispatcher.py
@@ -1,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+
+import pytest
+
+from app.xuanwu.hooks.runtime import (
+    HookHandlerDefinition,
+    HookRuntime,
+    HookRuntimeContext,
+)
+from app.xuanwu.hooks.runtime_models import HookDecision, HookEventEnvelope, HookEventType
+from app.xuanwu.hooks.runtime_sinks import ContextSink, MemorySink
+from app.xuanwu.hooks.runtime_store import HookStateStore
+
+
+@dataclass
+class _Recorder:
+    seen: list[str] = field(default_factory=list)
+
+    async def __call__(self, event, runtime_context):
+        self.seen.append(event.id)
+        await runtime_context.hook_state_store.append_event("audit", event)
+
+
+@pytest.mark.asyncio
+async def test_hook_runtime_dispatches_registered_handlers(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    runtime = HookRuntime(
+        HookRuntimeContext(
+            workspace_path=str(tmp_path),
+            hook_state_store=store,
+            memory_sink=MemorySink(str(tmp_path)),
+            context_sink=ContextSink(store),
+        )
+    )
+    recorder = _Recorder()
+    runtime.register(
+        HookHandlerDefinition(
+            module_name="audit",
+            event_types={HookEventType.RUN_STARTED},
+            handler=recorder,
+        )
+    )
+
+    event = HookEventEnvelope(
+        id="evt-1",
+        event_type=HookEventType.RUN_STARTED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-1",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"message": "hello"},
+    )
+
+    await runtime.dispatch(event)
+
+    assert recorder.seen == ["evt-1"]
+    stored = await store.list_events("audit", "user-a")
+    assert [item.id for item in stored] == ["evt-1"]
+
+
+@pytest.mark.asyncio
+async def test_hook_runtime_confirm_promotes_memory(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    runtime = HookRuntime(
+        HookRuntimeContext(
+            workspace_path=str(tmp_path),
+            hook_state_store=store,
+            memory_sink=MemorySink(str(tmp_path)),
+            context_sink=ContextSink(store),
+        )
+    )
+    event = HookEventEnvelope(
+        id="evt-2",
+        event_type=HookEventType.RUN_FAILED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-2",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"error": "failure"},
+    )
+
+    pending = await runtime.create_pending_item(
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=[event.id],
+        summary="Review failure",
+        payload={"error": "failure"},
+    )
+    resolved = await runtime.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id=pending.id,
+        decision=HookDecision.CONFIRM.value,
+        decided_by="user-a",
+        note="approved",
+    )
+
+    assert resolved.status.value == "confirmed"
+    memory_files = list((tmp_path / "users" / "user-a" / "memory").glob("memory_*.md"))
+    assert len(memory_files) == 1
+    content = memory_files[0].read_text(encoding="utf-8")
+    assert "Review failure" in content
+    confirmed_items = await runtime.context_sink.list_confirmed("audit", "user-a")
+    assert len(confirmed_items) == 1

tests/xuanwu/test_hook_runtime_sinks.py
@@ -1,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+
+import pytest
+
+from app.xuanwu.hooks.runtime_models import HookWriteMemoryRequest, PendingHookItem
+from app.xuanwu.hooks.runtime_sinks import ContextSink, MemorySink
+from app.xuanwu.hooks.runtime_store import HookStateStore
+
+
+@pytest.mark.asyncio
+async def test_memory_sink_writes_timestamped_memory_file(tmp_path):
+    sink = MemorySink(workspace_path=str(tmp_path))
+
+    result = await sink.write_confirmed(
+        HookWriteMemoryRequest(
+            module_name="audit",
+            user_id="user-a",
+            title="Promoted Lesson",
+            body="Remember this confirmed lesson.",
+            source_event_ids=["evt-1", "evt-2"],
+            metadata={"kind": "lesson"},
+        )
+    )
+
+    assert result.path.exists()
+    assert result.path.parent == tmp_path / "users" / "user-a" / "memory"
+    content = result.path.read_text(encoding="utf-8")
+    assert "Promoted Lesson" in content
+    assert "source_event_ids: evt-1, evt-2" in content
+    assert "kind: lesson" in content
+
+
+@pytest.mark.asyncio
+async def test_context_sink_exposes_confirmed_items_only(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    now = datetime.now(timezone.utc)
+    await store.append_pending(
+        "audit",
+        PendingHookItem(
+            id="pending-1",
+            module_name="audit",
+            user_id="user-a",
+            source_event_ids=["evt-1"],
+            summary="Confirmed item",
+            payload={"kind": "lesson"},
+            status="confirmed",
+            created_at=now,
+            updated_at=now,
+        ),
+    )
+    await store.append_pending(
+        "audit",
+        PendingHookItem(
+            id="pending-2",
+            module_name="audit",
+            user_id="user-a",
+            source_event_ids=["evt-2"],
+            summary="Still pending",
+            payload={"kind": "candidate"},
+            status="pending",
+            created_at=now,
+            updated_at=now,
+        ),
+    )
+
+    sink = ContextSink(store)
+    confirmed = await sink.list_confirmed("audit", "user-a")
+
+    assert len(confirmed) == 1
+    assert confirmed[0].summary == "Confirmed item"
+    assert confirmed[0].payload["kind"] == "lesson"
+
+
+@pytest.mark.asyncio
+async def test_context_sink_includes_explicit_injections(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    sink = ContextSink(store)
+
+    await sink.add_injection(
+        module_name="audit",
+        user_id="user-a",
+        summary="Recent preference",
+        payload={"body": "Prefer concise summaries"},
+        source_event_ids=["evt-3"],
+    )
+
+    confirmed = await sink.list_confirmed("audit", "user-a")
+
+    assert len(confirmed) == 1
+    assert confirmed[0].summary == "Recent preference"
+    assert confirmed[0].payload["body"] == "Prefer concise summaries"

tests/xuanwu/test_hook_runtime_store.py
@@ -1,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from datetime import datetime, timezone
+
+import pytest
+
+from app.xuanwu.hooks.runtime_models import (
+    HookEventEnvelope,
+    HookEventType,
+    PendingHookItem,
+)
+from app.xuanwu.hooks.runtime_store import HookStateStore
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_isolates_events_per_user(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    event = HookEventEnvelope(
+        id="evt-1",
+        event_type=HookEventType.RUN_STARTED,
+        user_id="user-a",
+        session_key="agent:main:user:user-a:main",
+        run_id="run-1",
+        channel="web",
+        agent_id="main",
+        created_at=datetime.now(timezone.utc),
+        payload={"message": "hello"},
+    )
+
+    await store.append_event("audit", event)
+
+    user_a_events = await store.list_events("audit", "user-a")
+    user_b_events = await store.list_events("audit", "user-b")
+
+    assert [item.id for item in user_a_events] == ["evt-1"]
+    assert user_b_events == []
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_tracks_pending_item_lifecycle(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    pending = PendingHookItem(
+        id="pending-1",
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=["evt-1"],
+        summary="Review the captured runtime anomaly",
+        payload={"kind": "anomaly"},
+        status="pending",
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc),
+    )
+
+    await store.append_pending("audit", pending)
+    initial_pending = await store.list_pending("audit", "user-a")
+    assert [item.id for item in initial_pending] == ["pending-1"]
+
+    confirmed = await store.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id="pending-1",
+        decision="confirm",
+        decided_by="user-a",
+        note="looks good",
+    )
+
+    assert confirmed.status == "confirmed"
+    assert await store.list_pending("audit", "user-a") == []
+
+    decisions = await store.list_decisions("audit", "user-a")
+    assert len(decisions) == 1
+    assert decisions[0].pending_id == "pending-1"
+    assert decisions[0].decision == "confirm"
+
+
+@pytest.mark.asyncio
+async def test_hook_state_store_reject_records_decision_and_hides_pending(tmp_path):
+    store = HookStateStore(workspace_path=str(tmp_path))
+    pending = PendingHookItem(
+        id="pending-2",
+        module_name="audit",
+        user_id="user-a",
+        source_event_ids=["evt-2"],
+        summary="Reject this candidate",
+        payload={"kind": "candidate"},
+        status="pending",
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc),
+    )
+
+    await store.append_pending("audit", pending)
+    rejected = await store.resolve_pending(
+        module_name="audit",
+        user_id="user-a",
+        pending_id="pending-2",
+        decision="reject",
+        decided_by="user-a",
+        note="not useful",
+    )
+
+    assert rejected.status == "rejected"
+    assert await store.list_pending("audit", "user-a") == []
+
+    decisions = await store.list_decisions("audit", "user-a")
+    assert len(decisions) == 1
+    assert decisions[0].decision == "reject"