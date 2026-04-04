cc-lichuyuan/atlasclaw

feat(users): implement user management redesign and account settings
- Redesign admin users page with enhanced UI layout and components
- Add account settings page with avatar upload support
- Add avatar upload API endpoint (JPG/PNG/WEBP/GIF, max 2MB)
- Enhance auth service with config snapshot loading
- Update header component for user management integration
- Add i18n translations for user management and account settings
- Add tests for user profile API, local auth, and frontend components

app/atlasclaw/api/api_routes.py
@@ -13,11 +13,15 @@ Note: Channel configuration is managed via /api/channels routes.
 
 from __future__ import annotations
 
+import re
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, File, HTTPException, Query, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.atlasclaw.core.config import get_config
 from app.atlasclaw.db import get_db_session_dependency as get_db_session
 from app.atlasclaw.db.schemas import (
     AgentCreate,
@@ -46,8 +50,21 @@ from app.atlasclaw.db.orm.service_provider_config import ServiceProviderConfigSe
 from app.atlasclaw.db.orm.user import UserService, verify_password
 from app.atlasclaw.auth.guards import get_current_user, require_admin
 from app.atlasclaw.auth.models import UserInfo
+from .services.auth_service import load_profile_snapshot
 from .model_config_routes import router as model_config_router
 from .provider_info_routes import router as provider_info_router
+
+ALLOWED_AVATAR_CONTENT_TYPES = {
+    "image/jpeg": ".jpg",
+    "image/png": ".png",
+    "image/webp": ".webp",
+    "image/gif": ".gif",
+}
+MAX_AVATAR_BYTES = 2 * 1024 * 1024
+
+
+def _is_local_auth_type(auth_type: str) -> bool:
+    return str(auth_type or "").strip().lower() == "local"
 
 
 router = APIRouter(prefix="/api", tags=["Database API"])
@@ -380,14 +397,16 @@ async def list_users(
 @router.get("/users/me/profile", response_model=UserResponse, status_code=200)
 async def get_my_profile(
     current_user: UserInfo = Depends(get_current_user),
-    session: AsyncSession = Depends(get_db_session),
 ) -> UserResponse:
     """Get the authenticated user's own profile."""
-    # current_user.user_id is the username from JWT, not database UUID
-    user = await UserService.get_by_username(session, current_user.user_id)
-    if not user:
+    profile = await load_profile_snapshot(
+        user_id=current_user.user_id,
+        auth_type=current_user.auth_type,
+        workspace_path=str(Path(get_config().workspace.path).resolve()),
+    )
+    if not profile:
         raise HTTPException(status_code=404, detail="User not found")
-    return UserResponse.model_validate(user)
+    return UserResponse.model_validate(profile)
 
 
 @router.put("/users/me/profile", response_model=UserResponse, status_code=200)
@@ -405,6 +424,11 @@ async def update_my_profile(
     # Get user by username first to get the database ID
     user = await UserService.get_by_username(session, current_user.user_id)
     if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Profile editing is not available for federated accounts",
+            )
         raise HTTPException(status_code=404, detail="User not found")
 
     # If email is being changed, check uniqueness
@@ -420,6 +444,55 @@ async def update_my_profile(
     return UserResponse.model_validate(updated)
 
 
+@router.post("/users/me/avatar", response_model=UserResponse, status_code=200)
+async def upload_my_avatar(
+    avatar: UploadFile = File(...),
+    current_user: UserInfo = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db_session),
+) -> UserResponse:
+    """Upload avatar image for the authenticated user."""
+    user = await UserService.get_by_username(session, current_user.user_id)
+    if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Avatar upload is not available for federated accounts",
+            )
+        raise HTTPException(status_code=404, detail="User not found")
+
+    content_type = (avatar.content_type or "").lower()
+    extension = ALLOWED_AVATAR_CONTENT_TYPES.get(content_type)
+    if not extension:
+        raise HTTPException(status_code=400, detail="Only JPG, PNG, WEBP, and GIF avatars are supported")
+
+    avatar_bytes = await avatar.read()
+    if not avatar_bytes:
+        raise HTTPException(status_code=400, detail="Avatar file is empty")
+
+    if len(avatar_bytes) > MAX_AVATAR_BYTES:
+        raise HTTPException(status_code=400, detail="Avatar image must be 2 MB or smaller")
+
+    workspace_path = Path(get_config().workspace.path).resolve()
+    avatar_dir = workspace_path / "public" / "avatars"
+    avatar_dir.mkdir(parents=True, exist_ok=True)
+
+    safe_username = re.sub(r"[^a-zA-Z0-9_-]", "_", user.username or user.id)
+    for existing_file in avatar_dir.glob(f"{safe_username}-*"):
+        if existing_file.is_file():
+            existing_file.unlink(missing_ok=True)
+
+    filename = f"{safe_username}-{datetime.utcnow().strftime('%Y%m%d%H%M%S')}{extension}"
+    avatar_path = avatar_dir / filename
+    avatar_path.write_bytes(avatar_bytes)
+
+    avatar_url = f"/user-content/avatars/{filename}"
+    updated = await UserService.update(session, user.id, UserUpdate(avatar_url=avatar_url))
+    if not updated:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return UserResponse.model_validate(updated)
+
+
 @router.put("/users/me/password", status_code=200)
 async def change_my_password(
     password_data: PasswordChange,
@@ -430,6 +503,11 @@ async def change_my_password(
     # Get user record first by username
     user = await UserService.get_by_username(session, current_user.user_id)
     if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Password authentication not available for this account",
+            )
         raise HTTPException(status_code=404, detail="User not found")
 
     # Verify current password

tests/atlasclaw/api/test_user_profile_api.py
@@ -10,9 +10,11 @@ Tests:
 from __future__ import annotations
 
 import asyncio
+import json
 from pathlib import Path
 from types import SimpleNamespace
 from typing import AsyncGenerator
+from unittest.mock import patch
 
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
@@ -20,6 +22,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.atlasclaw.api.api_routes import router as api_router
 from app.atlasclaw.api.routes import APIContext, create_router, set_api_context
+from app.atlasclaw.auth.guards import get_current_user
+from app.atlasclaw.auth.models import UserInfo
 from app.atlasclaw.auth.config import AuthConfig
 from app.atlasclaw.auth.middleware import setup_auth_middleware
 from app.atlasclaw.db import get_db_session
@@ -290,6 +294,126 @@ class TestUserProfileAPI:
 
         assert resp.status_code == 400
         assert "incorrect" in resp.json()["detail"].lower()
+
+        _cleanup_manager(manager)
+
+    def test_upload_avatar_success(self, tmp_path):
+        """User can upload avatar image and receives stored avatar URL."""
+        manager = _init_database_sync(tmp_path)
+        client = _build_client(tmp_path, _get_auth_config())
+        token = _login_as(client, "testuser", "testpass123")
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+
+        with patch(
+            "app.atlasclaw.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.post(
+                "/api/users/me/avatar",
+                files={"avatar": ("avatar.png", b"fake-png-data", "image/png")},
+                headers={"AtlasClaw-Authenticate": token},
+            )
+
+        assert resp.status_code == 200
+        data = resp.json()
+        assert data["avatar_url"].startswith("/user-content/avatars/testuser-")
+
+        avatar_files = list((workspace_path / "public" / "avatars").glob("testuser-*.png"))
+        assert len(avatar_files) == 1
+
+        _cleanup_manager(manager)
+
+    def test_upload_avatar_rejects_invalid_file_type(self, tmp_path):
+        """Avatar upload rejects non-image files."""
+        manager = _init_database_sync(tmp_path)
+        client = _build_client(tmp_path, _get_auth_config())
+        token = _login_as(client, "testuser", "testpass123")
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+
+        with patch(
+            "app.atlasclaw.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.post(
+                "/api/users/me/avatar",
+                files={"avatar": ("avatar.txt", b"not-an-image", "text/plain")},
+                headers={"AtlasClaw-Authenticate": token},
+            )
+
+        assert resp.status_code == 400
+        assert "supported" in resp.json()["detail"].lower()
+
+        _cleanup_manager(manager)
+
+    def test_get_shadow_user_profile(self, tmp_path):
+        """Shadow users can load a synthetic read-only profile."""
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+        (workspace_path / "users.json").write_text(
+            json.dumps(
+                {
+                    "users": [
+                        {
+                            "user_id": "shadow-user-1",
+                            "provider": "oidc",
+                            "subject": "sso-user@example.com",
+                            "display_name": "SSO User",
+                            "tenant_id": "default",
+                            "roles": ["user"],
+                            "auth_type": "oidc:test",
+                            "created_at": "2026-01-01T09:00:00+00:00",
+                            "last_seen_at": "2026-03-30T15:30:00+00:00",
+                        }
+                    ]
+                }
+            ),
+            encoding="utf-8",
+        )
+
+        app = FastAPI()
+        app.include_router(api_router)
+        app.dependency_overrides[get_current_user] = lambda: UserInfo(
+            user_id="shadow-user-1",
+            display_name="SSO User",
+            auth_type="oidc:test",
+            roles=["user"],
+        )
+
+        client = TestClient(app)
+
+        with patch(
+            "app.atlasclaw.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.get("/api/users/me/profile")
+
+        assert resp.status_code == 200
+        data = resp.json()
+        assert data["id"] == "shadow-user-1"
+        assert data["username"] == "sso-user@example.com"
+        assert data["display_name"] == "SSO User"
+        assert data["auth_type"] == "oidc:test"
+
+    def test_update_profile_unavailable_for_federated_account(self, tmp_path):
+        """Federated accounts cannot use local-only profile mutation endpoints."""
+        manager = _init_database_sync(tmp_path)
+        app = FastAPI()
+        app.include_router(api_router)
+        app.dependency_overrides[get_current_user] = lambda: UserInfo(
+            user_id="shadow-user-1",
+            display_name="SSO User",
+            auth_type="oidc:test",
+            roles=["user"],
+        )
+        app.dependency_overrides[get_db_session] = _test_get_db_session
+
+        client = TestClient(app)
+        resp = client.put("/api/users/me/profile", json={"display_name": "Updated"})
+
+        assert resp.status_code == 400
+        assert "federated" in resp.json()["detail"].lower()
 
         _cleanup_manager(manager)
 

tests/atlasclaw/test_local_auth_api.py
@@ -12,7 +12,7 @@ from app.atlasclaw.api.routes import APIContext, create_router, set_api_context
 from app.atlasclaw.auth.config import AuthConfig
 from app.atlasclaw.db.database import DatabaseConfig, init_database
 from app.atlasclaw.db.orm.user import UserService
-from app.atlasclaw.db.schemas import UserCreate
+from app.atlasclaw.db.schemas import UserCreate, UserUpdate
 from app.atlasclaw.session.manager import SessionManager
 from app.atlasclaw.session.queue import SessionQueue
 from app.atlasclaw.skills.registry import SkillRegistry
@@ -105,6 +105,46 @@ def test_auth_me_requires_valid_jwt(tmp_path):
         headers={"AtlasClaw-Authenticate": "bad-token"},
     )
     assert me_fail.status_code == 401
+
+    manager_cleanup(manager)
+
+
+def test_auth_me_includes_avatar_from_profile(tmp_path):
+    manager = init_database_sync(tmp_path)
+    client = _build_client(tmp_path)
+
+    async def _update_profile():
+        async with manager.get_session() as session:
+            user = await UserService.get_by_username(session, "admin")
+            await UserService.update(
+                session,
+                user.id,
+                UserUpdate(
+                    display_name="Atlas Admin",
+                    avatar_url="/user-content/avatars/admin-profile.png",
+                ),
+            )
+
+    import asyncio
+
+    asyncio.run(_update_profile())
+
+    login_resp = client.post(
+        "/api/auth/local/login",
+        json={"username": "admin", "password": "adminpass1"},
+    )
+    assert login_resp.status_code == 200
+    token = login_resp.json()["token"]
+
+    me_resp = client.get(
+        "/api/auth/me",
+        headers={"AtlasClaw-Authenticate": token},
+    )
+    assert me_resp.status_code == 200
+    body = me_resp.json()
+    assert body["display_name"] == "Atlas Admin"
+    assert body["avatar_url"] == "/user-content/avatars/admin-profile.png"
+    assert body["username"] == "admin"
 
     manager_cleanup(manager)
 

GrissonWu07/xuanwu

app/xuanwu/api/api_routes.py
@@ -13,11 +13,15 @@ Note: Channel configuration is managed via /api/channels routes.
 
 from __future__ import annotations
 
+import re
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, File, HTTPException, Query, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.xuanwu.core.config import get_config
 from app.xuanwu.db import get_db_session_dependency as get_db_session
 from app.xuanwu.db.schemas import (
     AgentCreate,
@@ -39,15 +43,29 @@ from app.xuanwu.db.schemas import (
     ProfileUpdate,
     PasswordChange,
 )
+from app.xuanwu.auth.guards import get_current_user, require_admin
+from app.xuanwu.auth.models import UserInfo
 from app.xuanwu.db.orm.agent_config import AgentConfigService
 from app.xuanwu.db.orm.audit import AuditService
 from app.xuanwu.db.orm.model_token_config import ModelTokenConfigService
 from app.xuanwu.db.orm.service_provider_config import ServiceProviderConfigService
 from app.xuanwu.db.orm.user import UserService, verify_password
-from app.xuanwu.auth.guards import get_current_user, require_admin
-from app.xuanwu.auth.models import UserInfo
+
+from .services.auth_service import load_profile_snapshot
 from .model_config_routes import router as model_config_router
 from .provider_info_routes import router as provider_info_router
+
+ALLOWED_AVATAR_CONTENT_TYPES = {
+    "image/jpeg": ".jpg",
+    "image/png": ".png",
+    "image/webp": ".webp",
+    "image/gif": ".gif",
+}
+MAX_AVATAR_BYTES = 2 * 1024 * 1024
+
+
+def _is_local_auth_type(auth_type: str) -> bool:
+    return str(auth_type or "").strip().lower() == "local"
 
 
 router = APIRouter(prefix="/api", tags=["Database API"])
@@ -380,14 +398,16 @@ async def list_users(
 @router.get("/users/me/profile", response_model=UserResponse, status_code=200)
 async def get_my_profile(
     current_user: UserInfo = Depends(get_current_user),
-    session: AsyncSession = Depends(get_db_session),
 ) -> UserResponse:
     """Get the authenticated user's own profile."""
-    # current_user.user_id is the username from JWT, not database UUID
-    user = await UserService.get_by_username(session, current_user.user_id)
-    if not user:
+    profile = await load_profile_snapshot(
+        user_id=current_user.user_id,
+        auth_type=current_user.auth_type,
+        workspace_path=str(Path(get_config().workspace.path).resolve()),
+    )
+    if not profile:
         raise HTTPException(status_code=404, detail="User not found")
-    return UserResponse.model_validate(user)
+    return UserResponse.model_validate(profile)
 
 
 @router.put("/users/me/profile", response_model=UserResponse, status_code=200)
@@ -405,6 +425,11 @@ async def update_my_profile(
     # Get user by username first to get the database ID
     user = await UserService.get_by_username(session, current_user.user_id)
     if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Profile editing is not available for federated accounts",
+            )
         raise HTTPException(status_code=404, detail="User not found")
 
     # If email is being changed, check uniqueness
@@ -420,6 +445,55 @@ async def update_my_profile(
     return UserResponse.model_validate(updated)
 
 
+@router.post("/users/me/avatar", response_model=UserResponse, status_code=200)
+async def upload_my_avatar(
+    avatar: UploadFile = File(...),
+    current_user: UserInfo = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db_session),
+) -> UserResponse:
+    """Upload avatar image for the authenticated user."""
+    user = await UserService.get_by_username(session, current_user.user_id)
+    if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Avatar upload is not available for federated accounts",
+            )
+        raise HTTPException(status_code=404, detail="User not found")
+
+    content_type = (avatar.content_type or "").lower()
+    extension = ALLOWED_AVATAR_CONTENT_TYPES.get(content_type)
+    if not extension:
+        raise HTTPException(status_code=400, detail="Only JPG, PNG, WEBP, and GIF avatars are supported")
+
+    avatar_bytes = await avatar.read()
+    if not avatar_bytes:
+        raise HTTPException(status_code=400, detail="Avatar file is empty")
+
+    if len(avatar_bytes) > MAX_AVATAR_BYTES:
+        raise HTTPException(status_code=400, detail="Avatar image must be 2 MB or smaller")
+
+    workspace_path = Path(get_config().workspace.path).resolve()
+    avatar_dir = workspace_path / "public" / "avatars"
+    avatar_dir.mkdir(parents=True, exist_ok=True)
+
+    safe_username = re.sub(r"[^a-zA-Z0-9_-]", "_", user.username or user.id)
+    for existing_file in avatar_dir.glob(f"{safe_username}-*"):
+        if existing_file.is_file():
+            existing_file.unlink(missing_ok=True)
+
+    filename = f"{safe_username}-{datetime.utcnow().strftime('%Y%m%d%H%M%S')}{extension}"
+    avatar_path = avatar_dir / filename
+    avatar_path.write_bytes(avatar_bytes)
+
+    avatar_url = f"/user-content/avatars/{filename}"
+    updated = await UserService.update(session, user.id, UserUpdate(avatar_url=avatar_url))
+    if not updated:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return UserResponse.model_validate(updated)
+
+
 @router.put("/users/me/password", status_code=200)
 async def change_my_password(
     password_data: PasswordChange,
@@ -430,6 +504,11 @@ async def change_my_password(
     # Get user record first by username
     user = await UserService.get_by_username(session, current_user.user_id)
     if not user:
+        if not _is_local_auth_type(current_user.auth_type):
+            raise HTTPException(
+                status_code=400,
+                detail="Password authentication not available for this account",
+            )
         raise HTTPException(status_code=404, detail="User not found")
 
     # Verify current password

tests/xuanwu/api/test_user_profile_api.py
@@ -10,9 +10,11 @@ Tests:
 from __future__ import annotations
 
 import asyncio
+import json
 from pathlib import Path
 from types import SimpleNamespace
 from typing import AsyncGenerator
+from unittest.mock import patch
 
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
@@ -20,6 +22,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.xuanwu.api.api_routes import router as api_router
 from app.xuanwu.api.routes import APIContext, create_router, set_api_context
+from app.xuanwu.auth.guards import get_current_user
+from app.xuanwu.auth.models import UserInfo
 from app.xuanwu.auth.config import AuthConfig
 from app.xuanwu.auth.middleware import setup_auth_middleware
 from app.xuanwu.db import get_db_session
@@ -290,6 +294,126 @@ class TestUserProfileAPI:
 
         assert resp.status_code == 400
         assert "incorrect" in resp.json()["detail"].lower()
+
+        _cleanup_manager(manager)
+
+    def test_upload_avatar_success(self, tmp_path):
+        """User can upload avatar image and receives stored avatar URL."""
+        manager = _init_database_sync(tmp_path)
+        client = _build_client(tmp_path, _get_auth_config())
+        token = _login_as(client, "testuser", "testpass123")
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+
+        with patch(
+            "app.xuanwu.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.post(
+                "/api/users/me/avatar",
+                files={"avatar": ("avatar.png", b"fake-png-data", "image/png")},
+                headers={"XuanWu-Authenticate": token},
+            )
+
+        assert resp.status_code == 200
+        data = resp.json()
+        assert data["avatar_url"].startswith("/user-content/avatars/testuser-")
+
+        avatar_files = list((workspace_path / "public" / "avatars").glob("testuser-*.png"))
+        assert len(avatar_files) == 1
+
+        _cleanup_manager(manager)
+
+    def test_upload_avatar_rejects_invalid_file_type(self, tmp_path):
+        """Avatar upload rejects non-image files."""
+        manager = _init_database_sync(tmp_path)
+        client = _build_client(tmp_path, _get_auth_config())
+        token = _login_as(client, "testuser", "testpass123")
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+
+        with patch(
+            "app.xuanwu.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.post(
+                "/api/users/me/avatar",
+                files={"avatar": ("avatar.txt", b"not-an-image", "text/plain")},
+                headers={"XuanWu-Authenticate": token},
+            )
+
+        assert resp.status_code == 400
+        assert "supported" in resp.json()["detail"].lower()
+
+        _cleanup_manager(manager)
+
+    def test_get_shadow_user_profile(self, tmp_path):
+        """Shadow users can load a synthetic read-only profile."""
+        workspace_path = tmp_path / "workspace"
+        workspace_path.mkdir(parents=True, exist_ok=True)
+        (workspace_path / "users.json").write_text(
+            json.dumps(
+                {
+                    "users": [
+                        {
+                            "user_id": "shadow-user-1",
+                            "provider": "oidc",
+                            "subject": "sso-user@example.com",
+                            "display_name": "SSO User",
+                            "tenant_id": "default",
+                            "roles": ["user"],
+                            "auth_type": "oidc:test",
+                            "created_at": "2026-01-01T09:00:00+00:00",
+                            "last_seen_at": "2026-03-30T15:30:00+00:00",
+                        }
+                    ]
+                }
+            ),
+            encoding="utf-8",
+        )
+
+        app = FastAPI()
+        app.include_router(api_router)
+        app.dependency_overrides[get_current_user] = lambda: UserInfo(
+            user_id="shadow-user-1",
+            display_name="SSO User",
+            auth_type="oidc:test",
+            roles=["user"],
+        )
+
+        client = TestClient(app)
+
+        with patch(
+            "app.xuanwu.api.api_routes.get_config",
+            return_value=SimpleNamespace(workspace=SimpleNamespace(path=str(workspace_path))),
+        ):
+            resp = client.get("/api/users/me/profile")
+
+        assert resp.status_code == 200
+        data = resp.json()
+        assert data["id"] == "shadow-user-1"
+        assert data["username"] == "sso-user@example.com"
+        assert data["display_name"] == "SSO User"
+        assert data["auth_type"] == "oidc:test"
+
+    def test_update_profile_unavailable_for_federated_account(self, tmp_path):
+        """Federated accounts cannot use local-only profile mutation endpoints."""
+        manager = _init_database_sync(tmp_path)
+        app = FastAPI()
+        app.include_router(api_router)
+        app.dependency_overrides[get_current_user] = lambda: UserInfo(
+            user_id="shadow-user-1",
+            display_name="SSO User",
+            auth_type="oidc:test",
+            roles=["user"],
+        )
+        app.dependency_overrides[get_db_session] = _test_get_db_session
+
+        client = TestClient(app)
+        resp = client.put("/api/users/me/profile", json={"display_name": "Updated"})
+
+        assert resp.status_code == 400
+        assert "federated" in resp.json()["detail"].lower()
 
         _cleanup_manager(manager)
 

tests/xuanwu/test_local_auth_api.py
@@ -12,7 +12,7 @@ from app.xuanwu.api.routes import APIContext, create_router, set_api_context
 from app.xuanwu.auth.config import AuthConfig
 from app.xuanwu.db.database import DatabaseConfig, init_database
 from app.xuanwu.db.orm.user import UserService
-from app.xuanwu.db.schemas import UserCreate
+from app.xuanwu.db.schemas import UserCreate, UserUpdate
 from app.xuanwu.session.manager import SessionManager
 from app.xuanwu.session.queue import SessionQueue
 from app.xuanwu.skills.registry import SkillRegistry
@@ -105,6 +105,46 @@ def test_auth_me_requires_valid_jwt(tmp_path):
         headers={"XuanWu-Authenticate": "bad-token"},
     )
     assert me_fail.status_code == 401
+
+    manager_cleanup(manager)
+
+
+def test_auth_me_includes_avatar_from_profile(tmp_path):
+    manager = init_database_sync(tmp_path)
+    client = _build_client(tmp_path)
+
+    async def _update_profile():
+        async with manager.get_session() as session:
+            user = await UserService.get_by_username(session, "admin")
+            await UserService.update(
+                session,
+                user.id,
+                UserUpdate(
+                    display_name="Atlas Admin",
+                    avatar_url="/user-content/avatars/admin-profile.png",
+                ),
+            )
+
+    import asyncio
+
+    asyncio.run(_update_profile())
+
+    login_resp = client.post(
+        "/api/auth/local/login",
+        json={"username": "admin", "password": "adminpass1"},
+    )
+    assert login_resp.status_code == 200
+    token = login_resp.json()["token"]
+
+    me_resp = client.get(
+        "/api/auth/me",
+        headers={"XuanWu-Authenticate": token},
+    )
+    assert me_resp.status_code == 200
+    body = me_resp.json()
+    assert body["display_name"] == "Atlas Admin"
+    assert body["avatar_url"] == "/user-content/avatars/admin-profile.png"
+    assert body["username"] == "admin"
 
     manager_cleanup(manager)
 